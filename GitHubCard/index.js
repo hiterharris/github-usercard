b8e4cf24ebc2b34f313eeaf90b3d058e4676ecfa
Hiter Harris <hiterharris@comcast.net>
2019-12-05 17:29:35 -0500

    code cleanup

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,48 +1,27 @@
-/* Step 1: using axios, send a GET request to the following URL 
-           (replacing the palceholder with your Github name):
-           https://api.github.com/users/<your name>
-*/
-
 axios.get('https://api.github.com/users/hiterharris/followers')
 
 .then(response => {
   const users = response.data.map(user => user);
   return users;
 })
+
 .then(result => {
   result.forEach(api => {
     axios.get(api.url)
     .then( response => {
       const followerCount = api.followers_url.length;
       const followingCount = api.following_url.length;
-      // console.log(followerCount);
       const newCard = Cards(response, followerCount, followingCount);
       entryPoint.appendChild(newCard);
     })
   })
 })
-  // .then( response => {
-  //   console.log(response);
-  //   response.data.forEach( item => {
-  //     const newFollower = Cards(item);
-  //     entryPoint.appendChild(newFollower);
-  //   })
-  // })
-  .catch( error => {
-    console.log('Data not returned', error);
-  });
 
-  const entryPoint = document.querySelector('.cards')
-/* Step 2: Inspect and study the data coming back, this is YOUR 
-   github info! You will need to understand the structure of this 
-   data in order to use it to build your component function 
+.catch( error => {
+  console.log('Data not returned', error);
+});
 
-   Skip to Step 3.
-*/
-
-/* Step 4: Pass the data received from Github into your function, 
-           create a new component and add it to the DOM as a child of .cards
-*/
+const entryPoint = document.querySelector('.cards')
 
 function Cards(response, followerCount, followingCount) {
 
@@ -74,18 +53,9 @@ function Cards(response, followerCount, followingCount) {
   profile.textContent = 'Profile: ';
   gitHub.href = `Profile: ${data.html_url}`;
   gitHub.textContent = `${data.html_url}`;
-
-
-  // ** TODO: get accurate data **
-  // followers.textContent = 'Followers:';
   followers.textContent = `Followers: ${followerCount}`;
   following.textContent = `Following: ${followingCount}`;
-
-
-  // following.textContent = 'Following:';
-  // following.textContent = `Following: ${followingCount}`;
   bio.textContent = 'Bio: ' ;
-  // **
 
 
   const cards = document.querySelector('.cards');
@@ -105,35 +75,3 @@ function Cards(response, followerCount, followingCount) {
 }
 
 Cards();
-
-/* Step 5: Now that you have your own card getting added to the DOM, either 
-          follow this link in your browser https://api.github.com/users/<Your github name>/followers 
-          , manually find some other users' github handles, or use the list found 
-          at the bottom of the page. Get at least 5 different Github usernames and add them as
-          Individual strings to the friendsArray below.
-          
-          Using that array, iterate over it, requesting data for each user, creating a new card for each
-          user, and adding that card to the DOM.
-*/
-
-
-
-/* Step 3: Create a function that accepts a single object as its only argument,
-          Using DOM methods and properties, create a component that will return the following DOM element:
-
-<div class="card">
-  <img src={image url of user} />
-  <div class="card-info">
-    <h3 class="name">{users name}</h3>
-    <p class="username">{users user name}</p>
-    <p>Location: {users location}</p>
-    <p>Profile:  
-      <a href={address to users github page}>{address to users github page}</a>
-    </p>
-    <p>Followers: {users followers count}</p>
-    <p>Following: {users following count}</p>
-    <p>Bio: {users bio}</p>
-  </div>
-</div>
-
-*/
