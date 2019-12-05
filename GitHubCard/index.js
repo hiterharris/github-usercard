ba50c24322f471d0f9a665eedeab0dbd0a7ccbb8
Hiter Harris <hiterharris@comcast.net>
2019-12-05 15:12:57 -0500

    created Cards component and followersArray

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,7 +3,13 @@
            https://api.github.com/users/<your name>
 */
 
-
+axios.get('https://api.github.com/users/hiterharris')
+  .then( response => {
+    console.log(response);
+  })
+  .catch( error => {
+    console.log('Data not returned', error);
+  });
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -16,6 +22,47 @@
            create a new component and add it to the DOM as a child of .cards
 */
 
+function Cards() {
+
+  // CREATING ELEMENTS
+  const card = document.createElement('div');
+  const userImg = document.createElement('img');
+  const cardInfo = document.createElement('div');
+  const name = document.createElement('h3');
+  const userName = document.createElement('p');
+  const location = document.createElement('p');
+  const profile = document.createElement('p');
+  const githubUrl = document.createElement('a');
+  const followers = document.createElement('p');
+  const following = document.createElement('p');
+  const bio = document.createElement('p');
+
+  // CREATING CLASS NAMES
+  card.classList.add('card');
+  cardInfo.classList.add('card-info');
+  name.classList.add('name');
+  userName.classList.add('username');
+
+  name.textContent = 'Name';
+
+  const cards = document.querySelector('.cards');
+  cards.appendChild(card);
+  card.appendChild(userImg);
+  card.appendChild(cardInfo);
+  cardInfo.appendChild(name);
+  cardInfo.appendChild(userName);
+  cardInfo.appendChild(location);
+  cardInfo.appendChild(profile);
+  profile.appendChild(githubUrl);
+  cardInfo.appendChild(followers);
+  cardInfo.appendChild(following);
+  cardInfo.appendChild(bio);
+
+  return card;
+}
+
+Cards();
+
 /* Step 5: Now that you have your own card getting added to the DOM, either 
           follow this link in your browser https://api.github.com/users/<Your github name>/followers 
           , manually find some other users' github handles, or use the list found 
@@ -26,7 +73,13 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = [
+  'TimJohnson',
+  'Mnunley1',
+  'brandonharris177',
+  'SandraCoburn',
+  'IsabellaGuo',
+];
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
